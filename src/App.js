2b0856a129709f2f89174bab6057893b0f45b183
Gina Harrold <blade836@gmail.com>
2022-08-24 12:04:26 -0400

    campsite directory page and campsite details

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import CampsitesList from './features/campsites/CampsitesList';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 //import React from 'react';
 //import { Container, Navbar, NavbarBrand } from 'reactstrap';
 //import NucampLogo from './app/assets/img/logo.png';
@@ -10,11 +10,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <Header />
-        <CampsitesList />
-        <Footer />
-    </div>
+      <div className='App'>
+          <Header />
+          <CampsitesDirectoryPage />
+          <Footer />
+      </div>
   );
 }
 
